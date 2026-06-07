457e2eca80320610fecc62b9a69cd9af233098cb
Sophie <sophie@Sophies-Mini.lan>
2026-06-07 16:32:21 +0300

    Add Lithuania operator-judgment signal

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const state = {
   filter: "all",
 };
 
-const assetVersion = "20260607-1030";
+const assetVersion = "20260607-1630";
 
 async function loadBoard() {
   const response = await fetch(`./data/signals.json?v=${assetVersion}`, { cache: "no-store" });
