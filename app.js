5c331713566100ee354c588080fbcb06d0c980d2
Sophie <sophie@Mac-2.lan>
2026-06-09 19:46:58 +0300

    Add GTM engineering role signal

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const state = {
   filter: "all",
 };
 
-const assetVersion = "20260609-1630";
+const assetVersion = "20260609-1945";
 
 async function loadBoard() {
   const response = await fetch(`./data/signals.json?v=${assetVersion}`, { cache: "no-store" });
