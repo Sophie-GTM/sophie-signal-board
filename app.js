1c74cedfa9019576e80208929d841bdbe2fd1493
Sophie <sophie@Mac-2.lan>
2026-06-29 10:33:42 +0300

    Add sales engineering proof work signal

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const state = {
   filter: "all",
 };
 
-const assetVersion = "20260628-1630";
+const assetVersion = "20260629-1030";
 
 async function loadBoard() {
   const response = await fetch(`./data/signals.json?v=${assetVersion}`, { cache: "no-store" });
