c0199a4ee6f2507d22c800f22cb964e6aeb72f0f
Sophie <sophie@Mac-2.lan>
2026-07-02 10:33:52 +0300

    Add replayable agent reliability signal

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const state = {
   filter: "all",
 };
 
-const assetVersion = "20260701-1630";
+const assetVersion = "20260702-1030";
 
 async function loadBoard() {
   const response = await fetch(`./data/signals.json?v=${assetVersion}`, { cache: "no-store" });
